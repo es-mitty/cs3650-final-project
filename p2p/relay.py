c50783e7fe28e42aaba8d6cda459dff2e1128501
es-mitty <esmitty1212@gmail.com>
2023-11-04 12:47:57 -0400

    override handle_client

--- p2p/relay.py
+++ p2p/relay.py
@@ -5,3 +5,7 @@ class Relay(Peer):
     """Relay used for handling all incoming and outgoing traffic in an unstructured P2P network."""
     def __init__(self, host, port):
         super().__init__(host, port)
+
+    def handle_client(self, connection, address):
+        """Plan to override to handle relay stuff."""
+        return super().handle_client(connection, address)
