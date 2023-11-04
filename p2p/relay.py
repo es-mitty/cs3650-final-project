43d2fd320b92919f1ea9d067facfad3e13259fa7
es-mitty <esmitty1212@gmail.com>
2023-11-04 12:51:37 -0400

    receive callback

--- p2p/relay.py
+++ p2p/relay.py
@@ -1,11 +1,16 @@
 from peer import Peer
+from typing import Callable
 
 
 class Relay(Peer):
     """Relay used for handling all incoming and outgoing traffic in an unstructured P2P network."""
     def __init__(self, host, port):
         super().__init__(host, port)
+        self.recv_callback = None
 
     def handle_client(self, connection, address):
         """Plan to override to handle relay stuff."""
         return super().handle_client(connection, address)
+
+    def set_recv_callback(self, callback: Callable):
+        self.recv_callback = callback
