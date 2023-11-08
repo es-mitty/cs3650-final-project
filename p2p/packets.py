fc3b607696a47e175811541b56fa6b90b9451594
es-mitty <esmitty1212@gmail.com>
2023-11-08 16:40:46 -0500

    PacketType enum

--- p2p/packets.py
+++ p2p/packets.py
@@ -1,19 +1,26 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Any
+from enum import Enum
+
+
+class PacketType(Enum):
+    CONNECTION_REQUEST = 0
+    CONNECTION_ACK = 1
+    MESSAGE = 2
+    REPORT = 3
+
 
 class Packet(ABC):
+
+    type: PacketType
+
     def __init__(self, data: dict[str, Any]):
         self.data = data
 
     @abstractmethod
     def encode(self):
         """Return an encoded version of `self.data` that can be sent over a socket."""
-        pass
-
-    @abstractmethod
-    @property
-    def data(self) -> dict:
         pass
 
 
