2c7aa575e0109eb498a9137ca6f45ae43e2f3ed8
es-mitty <esmitty1212@gmail.com>
2023-11-12 15:28:04 -0500

    packet factory

--- p2p/packets.py
+++ p2p/packets.py
@@ -1,25 +1,33 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Callable
 from enum import Enum
 
 
 class PacketType(Enum):
-    CONNECTION_REQUEST = 0
-    CONNECTION_ACK = 1
-    MESSAGE = 2
-    REPORT = 3
+    CONNECTION_REQUEST = 0xC
+    CONNECTION_ACK = 0xA
+    MESSAGE = 0xD
+    REPORT = 0xF
 
 
+@dataclass
 class Packet(ABC):
 
+    source_address: bytearray
+    destination_address: bytearray
     type: PacketType
 
-    def __init__(self, data: dict[str, Any]):
-        self.data = data
+    def __init__(self, from_bytes: bytearray | None = None):
+        if from_bytes:
+            assert len(from_bytes) >= 8
+            self.source_address = from_bytes[0:4]
+            self.destination_address = from_bytes[4:8]
+
 
     @abstractmethod
-    def encode(self):
+    def encode(self) -> bytearray:
         """Return an encoded version of `self.data` that can be sent over a socket."""
         pass
 
@@ -44,7 +52,17 @@ class UserReportPacket(Packet):
     type = PacketType.REPORT
 
 
-class RawPacketDecoder:
-    @staticmethod
-    def decode_raw_packet() -> Packet:
-        pass
+def packet_factory(from_bytes: bytearray) -> Packet:
+    """Given some decoded bytes from a TCP packet, return the appropriate data structure with decoded data."""
+    CLASS_MAP: dict[PacketType, Callable] = {
+            PacketType.CONNECTION_REQUEST: ConnectionRequestPacket,
+            PacketType.CONNECTION_ACK: ConnectionAcknowledgePacket,
+            PacketType.MESSAGE: MessagePacket,
+            PacketType.REPORT: UserReportPacket,
+        }
+    
+    assert len(from_bytes) >= 9
+
+    packet_type = PacketType(from_bytes[0])
+    packet_data = from_bytes[1:]
+    return CLASS_MAP[packet_type](from_bytes=packet_data)
