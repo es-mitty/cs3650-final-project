93d11d68764bd70dafd1ded72717f2089fc3d45d
es-mitty <esmitty1212@gmail.com>
2023-11-21 10:23:13 -0500

    Apply mapping to rest of code

--- p2p/p2p.py
+++ p2p/p2p.py
@@ -1,28 +1,28 @@
+from __future__ import annotations
+
 import time
 
 from peer import Peer
+from typing import TypedDict
 
+class UserEntry(TypedDict):
+    port: int
+    peer: Peer
 
-def main():
-    IPV4_ADDR = "0.0.0.0"
-    users = {
+def main() -> None:
+    ESTABLISHING_IPV4_ADDR = "0.0.0.0"
+    CONNECTION_IPV4_ADD = "127.0.0.1"
+    users: dict[str, UserEntry] = {
         "Anja": {"port": 8000, "peer": None},
         "Makayla": {"port": 8001, "peer": None},
         "Eli": {"port": 8002, "peer": None},
         "Murphy": {"port": 8003, "peer": None},
     }
 
-    Anja = Peer("0.0.0.0", 8000)
-    Anja.start()
-
-    Makayla = Peer("0.0.0.0", 8001)
-    Makayla.start()
-
-    Eli = Peer("0.0.0.0", 8002)
-    Eli.start()
-
-    Murphy = Peer("0.0.0.0", 8003)
-    Murphy.start()
+    for user in users:
+        peer = Peer(ESTABLISHING_IPV4_ADDR, users[user]["port"])
+        peer.start()
+        users[user]["peer"] = peer
 
     time.sleep(2)
 
@@ -32,45 +32,11 @@ def main():
         dest = input("Type who you want to send to: ")
         message = "https://api.thecatapi.com/v1/images/search?api_key=live_pGPtu252IVJfrslX8KvUkqhFOmid15pf8qL7r2PqXiPlnEwYzg7JWGC2MZdwTW4u"
 
-        if source == "Anja":
-            if dest == "Makayla":
-                Anja.connect("127.0.0.1", makayla_port)
-            elif dest == "Eli":
-                Anja.connect("127.0.0.1", eli_port)
-            elif dest == "Murphy":
-                Anja.connect("127.0.0.1", murphy_port)
+        if source in users and dest in users:
+            source_peer: Peer = users[source]["peer"]
+            source_peer.connect(CONNECTION_IPV4_ADD, users[dest]["port"])
             time.sleep(2)
-            Anja.send_data(message)
-
-        elif source == "Makayla":
-            if dest == "Anja":
-                Makayla.connect("127.0.0.1", anja_port)
-            elif dest == "Eli":
-                Makayla.connect("127.0.0.1", eli_port)
-            elif dest == "Murphy":
-                Makayla.connect("127.0.0.1", murphy_port)
-            time.sleep(2)
-            Makayla.send_data(message)
-
-        elif source == "Eli":
-            if dest == "Anja":
-                Eli.connect("127.0.0.1", anja_port)
-            elif dest == "Makayla":
-                Eli.connect("127.0.0.1", makayla_port)
-            elif dest == "Murphy":
-                Eli.connect("127.0.0.1", murphy_port)
-            time.sleep(2)
-            Eli.send_data(message)
-
-        elif source == "Murphy":
-            if dest == "Makayla":
-                Murphy.connect("127.0.0.1", makayla_port)
-            elif dest == "Eli":
-                Murphy.connect("127.0.0.1", eli_port)
-            elif dest == "Anja":
-                Murphy.connect("127.0.0.1", anja_port)
-            time.sleep(2)
-            Murphy.send_data(message)
+            source_peer.send_data(message)
 
         quit_demo = input("Type quit to quit the demo or anything else to continue: ")
 
